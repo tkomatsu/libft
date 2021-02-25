f7ca8d79b44fbfff0736abe441d0dd3087049c45
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-25 17:06:09 +0900

    fix(ft_free_split): ft_free -> free

--- srcs/string/ft_free_split.c
+++ srcs/string/ft_free_split.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 16:45:18 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/01/11 16:46:22 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/02/25 16:48:35 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	ft_free_split(char **strs)
 
 	i = 0;
 	while (strs[i])
-		ft_free(strs[i++]);
-	ft_free(strs);
+		free(strs[i++]);
+	free(strs);
 }
