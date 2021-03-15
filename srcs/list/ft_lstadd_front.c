3123f822e297bf435983c013ce5824e2be458562
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-03-15 21:10:58 +0900

    fix: error handling

--- srcs/list/ft_lstadd_front.c
+++ srcs/list/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/05 14:06:16 by tkomatsu          #+#    #+#             */
-/*   Updated: 2020/07/05 14:10:12 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/03/15 21:07:32 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (lst && new)
+	if (!lst || !new)
+		return ;
+	if (!(*lst))
+		*lst = new;
+	else
 	{
 		new->next = *lst;
 		*lst = new;
