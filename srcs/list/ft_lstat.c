c80ae2b3665941e09301138f71f625e446e99007
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-03-26 16:09:51 +0900

    fix(ft_lstat): return right value
    常に0を返してしまうバグを修正

--- srcs/list/ft_lstat.c
+++ srcs/list/ft_lstat.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 17:12:54 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/03/12 17:15:25 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/03/26 16:01:01 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstat(t_list *list_ptr, unsigned int nbr)
 	unsigned int i;
 
 	i = 0;
-	while (i != nbr && list_ptr)
+	while (i++ != nbr && list_ptr)
 		list_ptr = list_ptr->next;
 	return (list_ptr);
 }
